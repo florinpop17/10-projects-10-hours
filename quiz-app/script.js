9a9b3824b459a5d3703ae966625862582436ef7b
Florin Pop <popflorin1705@yahoo.com>
2020-09-02 11:38:04 +0300

    removed q

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,13 +1,5 @@
 const quizData = [
     {
-        question: "How old is Florin?",
-        a: "10",
-        b: "17",
-        c: "26",
-        d: "110",
-        correct: "c",
-    },
-    {
         question: "What is the most used programming language in 2019?",
         a: "Java",
         b: "C",
